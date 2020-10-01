820d60647ba43246eea68a26845872edca7a0d3f
Eliza Thompson <Eliza_thompson@icloud.com>
2020-10-01 16:16:38 +0100

    data pulling through site navigation working, route template for layout and route pages and files structure set up

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
 
-export default function Home() {
+export default function About() {
   return(
     <Layout>
-      <div>Home Page!</div>
+      <div>About Page!</div>
     </Layout>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
 
-export default function Home() {
+export default function Contact() {
   return(
     <Layout>
-      <div>Home Page!</div>
+      <div>Contact Page!</div>
     </Layout>
   )
 }
